809e67a5b02ac4e031ac39488e5487c19e216adb
Tom Clement <mr@tomclement.nl>
2015-10-22 19:41:20 +0200

    Added SC playback

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,21 +1,21 @@
+import _                            from 'lodash';
 import SoundCloud                   from 'soundcloud';
 import {Controller}                 from 'arva-js/core/Controller.js';
 import {MusicView}                  from '../views/MusicView.js';
 
 export class HomeController extends Controller {
 
-    Index(){
+    Index() {
+        let soundCloudClientID = '9adf30ed7e6f44f081e689d1f54dc7c6';
+
         SoundCloud.initialize({
-            client_id: '163eff0091a0cf8479d74a55e888b91b',
-            redirect_uri: 'http://localhost:63342/nl.bizboard.soundcloud-discovery/www/index.html#/Home/Callback'
+            client_id: soundCloudClientID
         });
 
-        return new Promise(function(resolve){
-            return SoundCloud.get('/tracks', {
-                q: 'buskers', license: 'cc-by-sa'
-            }).then(function(tracks) {
-                resolve(new MusicView({url:tracks[0].stream_url}));
-            });
+        return new Promise(async function (resolve) {
+            let tracks = await SoundCloud.get('/tracks'/*, {license: 'cc-by-sa' }*/);
+
+            resolve(new MusicView({url: `${_.sample(tracks).stream_url}?client_id=${soundCloudClientID}`}));
         });
     }
 }
