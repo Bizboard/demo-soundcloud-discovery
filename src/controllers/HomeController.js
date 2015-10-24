ebbb6e676489541a3c69bcdfd847dbf73de9fead
Hans van den Akker <hans@bizboard.nl>
2015-10-24 17:19:17 +0200

    Artwork prep Streamable filter

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -9,10 +9,11 @@ export class HomeController extends Controller {
     Index() {
 
         return new Promise(async function (resolve) {
-            let tracks = await (new SoundCloudFinder()).getRandomSongs();
-            let track = _.sample(tracks); //_.filter(tracks, (metaInfo) => metaInfo.license !== 'all-rights-reserved'));
+            let tracks = await (new SoundCloudFinder()).getRandomSongs({ limit: 100, genres: 'pop' });
+            let track = _.sample(_.filter(tracks, (track) => track.streamable)); //_.filter(tracks, (metaInfo) => metaInfo.license !== 'all-rights-reserved'));
 
             resolve(new MusicView({
+                artwork: track.artwork_url.replace('-large', '-t500x500'),
                 url: `${track.stream_url}?client_id=${SoundCloudClientID}`,
                 author: track.user.username,
                 title: track.title
--- src/views/MusicView.js
+++ src/views/MusicView.js
@@ -7,6 +7,7 @@ import Surface               from 'famous/core/Surface.js';
 import Utility               from 'famous/utilities/Utility.js';
 import {EqualizerSurface}    from '../components/EqualizerSurface.js';
 import {AudioSurface}        from '../components/AudioSurface.js';
+import {ImageSurface}        from '../components/ImageSurface.js';
 
 import {View}                from 'arva-js/core/View.js';
 
@@ -14,6 +15,12 @@ export class MusicView extends View {
 
     constructor(options = {}) {
         super(options);
+
+        this.renderables.artwork = new ImageSurface({
+            content: options.artwork,
+            sizeMode: ImageSurface.ASPECTFILL
+        });
+
 
         this.renderables.audio = new AudioSurface({
             url: options.url
@@ -36,6 +43,12 @@ export class MusicView extends View {
 
         this.layouts.push((context) => {
 
+            context.set('artwork', {
+                size: [context.size[0],context.size[1]/2],
+                origin: [0, 0],
+                align: [0, 0]
+            });
+
             context.set('audio', {
                 size: context.size,
                 origin: [0, 0],
@@ -43,14 +56,14 @@ export class MusicView extends View {
             });
 
             context.set('equalizerRight', {
-                size: [context.size[0]/2,context.size[1]],
-                origin: [1, 0.5],
-                align: [1, 0.5]
+                size: [context.size[0]/2,context.size[1]/2],
+                origin: [1, 1],
+                align: [1, 1]
             });
             context.set('equalizerLeft', {
-                size: [context.size[0]/2,context.size[1]],
-                origin: [0, 0.5],
-                align: [0, 0.5]
+                size: [context.size[0]/2,context.size[1]/2],
+                origin: [0, 1],
+                align: [0, 1]
             });
 
         });
